c7a9946aac705f7b36808b16ea472ed5e3face0b
onouek <kohsuke.onoue@gmail.com>
2018-06-27 19:23:48 +0900

    Change db sqlite to psql

--- django/config/settings.py
+++ django/config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'answerly',
+        'USER': 'answerly',
+        'PASSWORD': 'development',
+        'PORT': 5432,
+        'HOST': '127.0.0.1',
     }
 }
 
