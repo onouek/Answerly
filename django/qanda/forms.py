cb4f3fe3b1f4dc80de76f4e381e010f401223331
onouek <kohsuke.onoue@gmail.com>
2018-06-27 20:16:28 +0900

    Add question form

---
+++ django/qanda/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from quanda.model import Question
+
+
+class QuestionForm(forms.ModelForm):
+    user = forms.ModelChoiceField(
+        widget=forms.HiddenInput,
+        queryset=get_user_model().objects.all(),
+        disabled=True,
+    )
+
+    class Meta:
+        model = Question
+        fields = ['title', 'question', 'user', ]
