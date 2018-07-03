ed763b7d789068e1e6f7fa735af28a7e29666a88
onouek <kohsuke.onoue@gmail.com>
2018-07-03 22:38:06 +0900

    Add answer form

--- django/qanda/forms.py
+++ django/qanda/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from qanda.models import Question
+from qanda.models import Question, Answer
 
 
 class QuestionForm(forms.ModelForm):
@@ -13,3 +13,20 @@ class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ['title', 'question', 'user', ]
+
+
+class AnswerForm(forms.ModelForm):
+    user = forms.ModelChoiceField(
+        widget=forms.HiddenInput,
+        queryset=get_user_model().objects.all(),
+        disabled=True,
+    )
+    question = forms.ModelChoiceField(
+        widget=forms.HiddenInput,
+        queryset=Question.objects.all(),
+        disabled=True,
+    )
+
+    class Meta:
+        model = Answer
+        fields = ['answer', 'user', 'question', ]
