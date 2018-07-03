93f0652756782c648fff85e4d902d37ce66ce889
onouek <kohsuke.onoue@gmail.com>
2018-07-03 23:46:53 +0900

    Add user register view

--- django/user/views.py
+++ django/user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+
+
+class RegisterView(CreateView):
+    template_name = 'user/register.html'
+    form_class = UserCreationForm
