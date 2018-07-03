ba71b63b41349f22b4e920938a46148e298731aa
onouek <kohsuke.onoue@gmail.com>
2018-07-03 22:55:10 +0900

    Route requests to question detail view

--- django/qanda/urls.py
+++ django/qanda/urls.py
@@ -4,4 +4,7 @@ from qanda import views
 app_name = 'qanda'
 urlpatterns = [
     path('ask', views.AskQuestionView.as_view(), name='ask'),
+    path('q/<int:pk>',
+    views.QuestionDetailView.as_view(),
+    name='question_detail'),
 ]
