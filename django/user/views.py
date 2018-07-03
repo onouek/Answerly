74e308ba481640bb299c928b5bc5d29d8be73851
onouek <kohsuke.onoue@gmail.com>
2018-07-04 00:23:07 +0900

    Add success_url to user register view

--- django/user/views.py
+++ django/user/views.py
@@ -3,8 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
+from django.urls import (
+    reverse_lazy,
+)
 
 
 class RegisterView(CreateView):
     template_name = 'user/register.html'
     form_class = UserCreationForm
+    success_url = reverse_lazy(
+        'qanda:index')
