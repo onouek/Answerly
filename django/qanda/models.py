c648fc21546f55d14c7ebd4914dc784bd9502b88
onouek <kohsuke.onoue@gmail.com>
2018-07-04 20:44:34 +0900

    Add Question into elasticsearch on save

--- django/qanda/models.py
+++ django/qanda/models.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.db import models
 from django.urls.base import reverse
 
+from qanda.service import elasticsearch
+
 
 class Question(models.Model):
     title = models.CharField(max_length=140)
@@ -12,6 +14,14 @@ class Question(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        super().save(force_insert=force_insert,
+                     force_update=force_update,
+                     using=using,
+                     update_fields=update_fields)
+        elasticsearch.upsert(self)
 
     def get_absolute_url(self):
         return reverse('qanda:question_detail', kwargs={'pk': self.id})
