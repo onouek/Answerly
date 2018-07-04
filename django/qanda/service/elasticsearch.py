cfc9251b431939ee2d103f2b562e2f6b49da2977
onouek <kohsuke.onoue@gmail.com>
2018-07-04 20:07:09 +0900

    Create elasticsearch service

---
+++ django/qanda/service/elasticsearch.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from elasticsearch import Elasticsearch, TransportError
+from elasticsearch.helpers import streaming_bulk
+
+from django.conf import settings
+
+import logging
+
+ALREADY_EXISTS_EXCEPTION = 'resource_already_exists_exception'
+
+FAILED_TO_LOAD_ERROR = 'Failed to load {}: {!r}'
+
+ISO_DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
+
+logger = logging.getLogger(__name__)
+
+
+def get_client():
+    return Elasticsearch(hosts=[
+        {'host': settings.ES_HOST, 'port': settings.ES_PORT,}
+    ])
+
+def create_index(name=settings.ES_INDEX):
+    client = get_client()
+    if not client.indices.exists(index=name):
+        try:
+            client.indices.create(index=name)
+        except TransportError as e:
+            if e.error == ALREADY_EXISTS_EXCEPTION:
+                pass
+            else:
+                raise e
+
+def bulk_load(questions):
+    all_ok = True
+    es_questions = (q.as_elasticsearch_dict() for q in questions)
+    for ok, result in streaming_bulk(
+            get_client(),
+            es_questions,
+            index=settings.ES_INDEX,
+            raise_on_error=False,
+    ):
+        if not ok:
+            all_ok = False
+            action, result = result.popitem()
+            logger.error(FAILED_TO_LOAD_ERROR.format(result['_id'], result))
+    return all_ok
