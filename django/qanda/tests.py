39c9d5b8b7327c38b0f05b9584c0ce78954c77d3
onouek <kohsuke.onoue@gmail.com>
2018-07-11 19:39:58 +0900

    Create Questio.save test case

--- django/qanda/tests.py
+++ django/qanda/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from unittest.mock import patch
 
-# Create your tests here.
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from elasticsearch import Elasticsearch
+
+from qanda.models import Question
+
+QUESTION_CREATED_STRFTIME = '%Y-%m-%d %H:%M'
+
+
+class QuestionSaveTestCase(TestCase):
+    """
+    Tests Question.save()
+    """
+
+    @patch('qanda.service.elasticsearch.Elasticsearch')
+    def test_elasticsearch_upsert_on_save(self, ElasticsearchMock):
+        user = get_user_model().objects.create_user(
+            username='unittest',
+            password='unittest',
+        )
+        question_title = 'Unit test'
+        question_body = 'some long text'
+        q = Question(
+            title=question_title,
+            question=question_body,
+            user=user,
+        )
+        q.save()
+
+        self.assertIsNotNone(q.id)
+        self.assertTrue(ElasticsearchMock.called)
+        mock_client = ElasticsearchMock.return_value
+        mock_client.update.assert_called_once_with(
+            settings.ES_INDEX,
+            'doc',
+            id=q.id,
+            body={
+                'doc': {
+                    'text': '{}\n{}'.format(question_title, question_body),
+                    'question_body': question_body,
+                    'title': question_title,
+                    'id': q.id,
+                    'created': q.created,
+                },
+                'doc_as_upsert': True,
+            }
+        )
