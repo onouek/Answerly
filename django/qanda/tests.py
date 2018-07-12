9a97701d8f03185002974c8a89bd551f1da4bc03
onouek <kohsuke.onoue@gmail.com>
2018-07-12 20:07:03 +0900

    Add daily question list test case

--- django/qanda/tests.py
+++ django/qanda/tests.py
@@ -1,11 +1,13 @@
+from datetime import date
 from unittest.mock import patch
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from elasticsearch import Elasticsearch
-
+from qanda.factories import QuestionFactory
 from qanda.models import Question
+from qanda.views import DailyQuestionList
 
 QUESTION_CREATED_STRFTIME = '%Y-%m-%d %H:%M'
 
@@ -48,3 +50,55 @@ class QuestionSaveTestCase(TestCase):
                 'doc_as_upsert': True,
             }
         )
+
+
+class DailyQuestionListTestCase(TestCase):
+    """
+    Tests the DailyQuestionList view
+    """
+    QUESTION_LIST_NEEDLE_TEMPLATE = '''
+    <li >
+        <a href="/q/{id}" >{title}</a >
+        by {username} on {date}
+    </li >
+    '''
+
+    REQUEST = RequestFactory().get(path='/q/2030-12-31')
+    today = date.today()
+
+    def test_GET_on_day_with_no_questions(self):
+        response = DailyQuestionList.as_view()(
+            self.REQUEST,
+            year=self.today.year,
+            month=self.today.month,
+            day=self.today.day
+        )
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(['qanda/question_archive_day.html'],
+                         response.template_name)
+        self.assertEqual(0, response.context_data['object_list'].count())
+        self.assertContains(response,
+                            'Hmm... Everyone thinks they know everything '
+                            'today.')
+    
+    def test_GET_on_day_with_many_questions(self):
+        todays_questions = [QuestionFactory() for _ in range(10)]
+
+        response = DailyQuestionList.as_view()(
+            self.REQUEST,
+            year=self.today.year,
+            month=self.today.month,
+            day=self.today.day
+        )
+
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(10, response.context_data['object_list'].count())
+        rendered_content = response.rendered_content
+        for question in todays_questions:
+            needle = self.QUESTION_LIST_NEEDLE_TEMPLATE.format(
+                id=question.id,
+                title=question.title,
+                username=question.user.username,
+                date=question.created.strftime(QUESTION_CREATED_STRFTIME)
+            )
+            self.assertInHTML(needle, rendered_content)
